fb00d9a0bc74acf341cc8ec2737ea9eba5309bfb
1686 <81729493+1686-st@users.noreply.github.com>
2026-07-05 18:57:50 +0900

    Update main.js

--- gs_test/main.js
+++ gs_test/main.js
@@ -1,13 +1,10 @@
 const newsSheelUrl =
   "https://docs.google.com/spreadsheets/d/18Xjbji7idrD-JtV8cCicn0GVsVhYEAiFSotFel2P49E/gviz/tq?tqx=out:csv";
-
 const parseCsv = (csvString) => {
   const rows = csvString.split("\n");
   return rows.map((row) => {
-    const [date, content, test1, test2, test3] = row
-      .split(",")
-      .map((col) => col.slice(1, -1));
-    return { date, content, test1, test2, test3 };
+    const [date, content] = row.split(",").map((col) => col.slice(1, -1));
+    return { date, content };
   });
 };
 
@@ -18,15 +15,12 @@ const parseCsv = (csvString) => {
 
   parseCsv(content)
     .reverse()
-    .forEach(({ date, content, test1, test2, test3 }) => {
+    .forEach(({ date, content }) => {
       const tableRow = document.createElement("tr");
       const dateCell = document.createElement("td");
       dateCell.textContent = date;
       const contentCell = document.createElement("td");
       contentCell.textContent = content;
-      contentCell.textContent = test1;
-      contentCell.textContent = test2;
-      contentCell.textContent = test3;
       tableRow.appendChild(dateCell);
       tableRow.appendChild(contentCell);
       newsTableBody.appendChild(tableRow);
