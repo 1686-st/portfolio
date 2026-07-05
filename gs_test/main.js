1d5ef291da2af102bd6a405ecd92bc809bc6a1b7
1686 <81729493+1686-st@users.noreply.github.com>
2026-07-05 18:30:24 +0900

    Update main.js

--- gs_test/main.js
+++ gs_test/main.js
@@ -4,8 +4,10 @@ const newsSheelUrl =
 const parseCsv = (csvString) => {
   const rows = csvString.split("\n");
   return rows.map((row) => {
-    const [date, content] = row.split(",").map((col) => col.slice(1, -1));
-    return { date, content };
+    const [date, content, test1, test2, test3] = row
+      .split(",")
+      .map((col) => col.slice(1, -1));
+    return { date, content, test1, test2, test3 };
   });
 };
 
@@ -16,12 +18,27 @@ const parseCsv = (csvString) => {
 
   parseCsv(content)
     .reverse()
-    .forEach(({ date, content }) => {
+    .forEach(({ date, content, test1, test2, test3 }) => {
       const tableRow = document.createElement("tr");
       const dateCell = document.createElement("td");
       dateCell.textContent = date;
       const contentCell = document.createElement("td");
       contentCell.textContent = content;
+      tableRow.appendChild(dateCell);
+      tableRow.appendChild(contentCell);
+      newsTableBody.appendChild(tableRow);
+      const contentCell = document.createElement("td");
+      contentCell.textContent = test1;
+      tableRow.appendChild(dateCell);
+      tableRow.appendChild(contentCell);
+      newsTableBody.appendChild(tableRow);
+      const contentCell = document.createElement("td");
+      contentCell.textContent = test2;
+      tableRow.appendChild(dateCell);
+      tableRow.appendChild(contentCell);
+      newsTableBody.appendChild(tableRow);
+      const contentCell = document.createElement("td");
+      contentCell.textContent = test3;
       tableRow.appendChild(dateCell);
       tableRow.appendChild(contentCell);
       newsTableBody.appendChild(tableRow);
