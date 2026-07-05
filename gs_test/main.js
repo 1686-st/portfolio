58fbe9ca4148c06ef49a76af5c511a9bfbc39367
1686 <81729493+1686-st@users.noreply.github.com>
2026-07-05 18:17:26 +0900

    gs_test

---
+++ gs_test/main.js
@@ -0,0 +1,29 @@
+const newsSheelUrl =
+  "https://docs.google.com/spreadsheets/d/18Xjbji7idrD-JtV8cCicn0GVsVhYEAiFSotFel2P49E/gviz/tq?tqx=out:csv";
+
+const parseCsv = (csvString) => {
+  const rows = csvString.split("\n");
+  return rows.map((row) => {
+    const [date, content] = row.split(",").map((col) => col.slice(1, -1));
+    return { date, content };
+  });
+};
+
+(async () => {
+  const newsTableBody = document.getElementById("newsTableBody");
+  const response = await fetch(newsSheelUrl);
+  const content = await response.text();
+
+  parseCsv(content)
+    .reverse()
+    .forEach(({ date, content }) => {
+      const tableRow = document.createElement("tr");
+      const dateCell = document.createElement("td");
+      dateCell.textContent = date;
+      const contentCell = document.createElement("td");
+      contentCell.textContent = content;
+      tableRow.appendChild(dateCell);
+      tableRow.appendChild(contentCell);
+      newsTableBody.appendChild(tableRow);
+    });
+})();
